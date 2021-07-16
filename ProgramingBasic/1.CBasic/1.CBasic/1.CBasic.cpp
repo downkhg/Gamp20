5e590baa21d533db79a8f86a863b81fc4cea909c
downkhg <downkhg@gmail.com>
2021-07-16 16:24:37 +0900

    실수와 정수

--- ProgramingBasic/1.CBasic/1.CBasic/1.CBasic.cpp
+++ ProgramingBasic/1.CBasic/1.CBasic/1.CBasic.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>//표준입출력
 
 //원의 넓이를 구하는 프로그램만들기(단, 원의 넓이는 입력을 받고, 구한 원의넓이를 출력하라)
-//반지름*반지름*3.14 = 원의 넓이 //정수:int 실수:float
+//반지름*반지름*3.14 = 원의 넓이 //정수:int 실수:float 변수:데이터를 담는 것
 //데이터(재료): 반지름, 3.14 = Pi
 //알고리즘(레시피): 반지름*반지름*3.14 = 원의 넓이, 구한 원의 넓이를 출력하라.
 void CircleAreaMain()
@@ -10,10 +10,10 @@ void CircleAreaMain()
 	//2.변수 파이를 만들고 값을 저장한다.
 	//3.원의 넓이를 반지름*반지름*3.14을 이용하여 구한다.
 	//4.원의 넓이를 출력한다.
-	int radius = 5;//F9:중단점설정
+	float radius = 1;//F9:중단점설정
 	float pi = 3.14;
-	int area = radius * radius * pi;
-	printf("Area:%d\n",area);
+	float area = radius * radius * pi;
+	printf("Area:%f\n",area);
 }
 
 void main()
