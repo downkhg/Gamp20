22e4b455b2392a4bcbbe0bde60409bdf60bfa5ed
downkhg <downkhg@gmail.com>
2021-08-04 21:03:07 +0900

    템플릿

--- ProgramingBasic/5.string/5.string/5.string.cpp
+++ ProgramingBasic/5.string/5.string/5.string.cpp
@@ -2,12 +2,12 @@
 //문자열: 문자배열
 void StringTestMain()
 {
-	const int  SIZE = 3;
-	char strInput[SIZE];
+	const int  SIZE = 4;
+	char strInput[SIZE] = "abc";//문자열초기화시 문자열의 끝에는 반드시 널문자가 삽입된다.
 	
-	strInput[0] = 'a';
+	/*strInput[0] = 'a';
 	strInput[1] = 'b';
-	strInput[2] = 'c';
+	strInput[2] = 'c';*/
 	//strInput[3] = '\0';//널문자: 문자열의 끝을알리는 값.
 
 	for (int i = 0; i < SIZE; i++)
@@ -24,7 +24,7 @@ void StringTestMain()
 	printf("Size:%d\n",nSize);
 	printf("%s",strInput);
 }
-
+//널문자 필요한이유? 메모리의 크기는 여유롭게 줄수있지만 실제 문자열의 길이는 그보다 작을수도있으므로, 불필요한 출력을 하지않기위해서.
 void InputIDMain()
 {
 	const int SIZE = 24;
@@ -44,6 +44,17 @@ void InputIDMain()
 	}
 	printf("\n");
 	printf("%s",strID);
+}
+//Q:____
+//A:A
+//Q:_A__
+//A:G
+//Q:GA__
+//.........
+//A:GAME
+void HangMainMain()
+{
+
 }
 
 void main()
