e92fc16632832d26db5fbfc34c0df649b20d0298
downkhg <downkhg@gmail.com>
2021-08-10 17:35:47 +0900

    함수포인터 풀이

--- ProgramingBasic/7.FunctionPtr/7.FunctionPtr/7.FunctionPtr.cpp
+++ ProgramingBasic/7.FunctionPtr/7.FunctionPtr/7.FunctionPtr.cpp
@@ -22,7 +22,7 @@ int Div(int a, int b)
 //함수포인터: 함수의 주소값을 저장하는 변수 -> 함수도 주소가 있다.
 void FuncPtrTestMain()
 {
-	//리턴값 (*포인터명)(매개변수,매계변수): 리턴값, 매계변수의 개수와 타입이 같아야한다.
+	//리턴값 (*포인터명)(매개변수,매개변수): 리턴값, 매개변수의 개수와 타입이 같아야한다.
 	int (*pfn)(int, int);
 
 	pfn = Add;
@@ -39,16 +39,54 @@ void EmptyMain()
 {
 	printf("EmptyMain!\n");
 }
+
+
+float Add(float a, float b, float c)
+{
+	return a + b + c;
+}
+
+float Sub(float a, float b, float c)
+{
+	return a - b - c;
+}
+
+float Multiple(float a, float b, float c)
+{
+	return a * b * c;
+}
+
+float Div(float a, float b, float c)
+{
+	return a / b / c;
+}
+
 //사칙연산이 가능한 3개의 매개변수를 실수로 받아 계산하는 프로그램 작성하기
 void CaculatorMain()
 {
+	//리턴값 (*포인터명)(매개변수,매개변수,매개변수): 리턴값, 매계변수의 개수와 타입이 같아야한다.
+	float (*pFn)(float,float, float);
 
+	pFn = Add;
+	printf("Add[%d]:%f\n",pFn, pFn(10, 20, 30));
+	pFn = Sub;
+	pFn(10, 20, 30);
+	printf("Sub[%d]:%f\n", pFn, pFn(10, 20, 30));
+	pFn = Multiple;
+	pFn(10, 20, 30);
+	printf("Multiple[%d]:%f\n", pFn, pFn(10, 20, 30));
+	pFn = Div;
+	pFn(10, 20, 30);
+	printf("Div[%d]:%f\n", pFn, pFn(10, 20, 30));
 }
+
 void main()
 {
 	void (*pMain)();
 	pMain = FuncPtrTestMain;
 	pMain();
 	pMain = EmptyMain;
+	pMain();
+	pMain = CaculatorMain;
 	pMain();
 }
