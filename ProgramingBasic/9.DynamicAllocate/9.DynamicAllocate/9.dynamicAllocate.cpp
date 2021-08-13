03987c38f4affd2a55f41a4f9127a8c8bddd1df6
downkhg <downkhg@gmail.com>
2021-08-13 17:50:32 +0900

    동적할당과 리스트

---
+++ ProgramingBasic/9.DynamicAllocate/9.DynamicAllocate/9.dynamicAllocate.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+//정적할당: 프로그램 실행전에 메모리가 할당되는 것. 
+//동적할당: 프로그램실행(런타임)중에 메모리를 할당하는 것.
+//void포인터: 비어있다. 타입이없는 포인터
+//동적할당된 메모리는 컴파일러가 관리할수없으므로, 프로그래머가 직접 해제해줘야한다.
+//만약, 하지않으면 메모리 누수가 발생하게된다.
+void DynamicAllocateMain()
+{
+	int* pData = new int();//(int*)malloc(sizeof(int)); //c
+	*pData = 10;
+	printf("pData[%d]:%d\n",pData, *pData);
+	//메모리삭제
+	delete pData; 
+	//free(pData);//c
+}
+void DynamicAllocateArrayMain()
+{
+	int nSize;
+	printf("ArraySize:");
+	scanf("%d",&nSize);
+	int* pData = new int[nSize];//(int*)malloc(sizeof(int) * nSize);
+	for (int i = 0; i < nSize; i++)
+	{
+		pData[i] = i * 10;
+		printf("pData[%d]:%d\n", i, pData[i]);
+	}	
+	//메모리삭제
+	delete[] pData;
+	//free(pData);
+}
+void main()
+{
+	DynamicAllocateMain();
+	DynamicAllocateArrayMain();
+}
