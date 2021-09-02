e6857ac608c8e033584b66cf41a7a332faee53df
downkhg <downkhg@gmail.com>
2021-09-02 17:57:42 +0900

    STL: 큐,스택,우선순위큐,셋,맵,해시맵 트리: 힙트리삽입, 셋테스트

--- ProgramingBasic/DataStruct/2.BinaryTree/2.BinaryTree/BinaryTree.cpp
+++ ProgramingBasic/DataStruct/2.BinaryTree/2.BinaryTree/BinaryTree.cpp
@@ -43,9 +43,9 @@ void Traverse(SNode* pNode)
 	if (!pNode) return;
 	//printf("%d\n", pNode->nData); //전위
 	Traverse(pNode->pLeft);
-	//printf("%d\n", pNode->nData); //중위
+	printf("%d\n", pNode->nData); //중위
 	Traverse(pNode->pRight);
-	printf("%d\n", pNode->nData); //후위
+	//printf("%d\n", pNode->nData); //후위
 }
 
 void Print(SNode* pSeed)
@@ -53,7 +53,67 @@ void Print(SNode* pSeed)
 	Traverse(pSeed);
 }
 
-void main()
+void Swap(int& a, int& b)
+{
+	printf("Swap[%d <->%d]\n", a, b);
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void MinHipTreeInsert(SNode* pParent, SNode* pInsert)
+{
+	if (pParent == NULL) return;
+	if (pParent->nData > pInsert->nData)
+	{
+		printf("Swap Parent\n");
+		Swap(pParent->nData, pInsert->nData);
+
+		MinHipTreeInsert(pParent, pInsert);
+	}
+	else
+	{
+		if (pParent->pLeft == NULL)
+		{
+			pParent->pLeft = pInsert;
+			printf("MinHipTreeInsert_Left:%d\n", pInsert->nData);
+			return;
+		}
+		else if (pParent->pRight == NULL)
+		{
+			pParent->pRight = pInsert;
+			printf("MinHipTreeInsert_Right:%d\n", pInsert->nData);
+			return;
+		}
+		else
+			MinHipTreeInsert(pParent->pLeft, pInsert);
+	}
+}
+
+void SetTestMain()
+{
+	const int nSize = 7;
+	SNode* arrNode[nSize];
+
+	for (int i = 0; i < nSize; i++)
+		arrNode[i] = CreateNode((i + 1) * 10);
+
+	SNode* pSeed = arrNode[3];
+	
+	MakeLeft(arrNode[3], arrNode[1]);
+	MakeRight(arrNode[3], arrNode[5]);
+
+
+	MakeLeft(arrNode[1], arrNode[0]);
+	MakeRight(arrNode[1], arrNode[2]);
+
+	MakeLeft(arrNode[5], arrNode[4]);
+	MakeRight(arrNode[5], arrNode[6]);
+
+	Print(pSeed);
+}
+
+void TreeBasicMain()
 {
 	SNode* pSeed = NULL;
 
@@ -62,6 +122,7 @@ void main()
 	SNode* pRight = CreateNode(30);
 	SNode* pD = CreateNode(40);
 	SNode* pE = CreateNode(50);
+
 
 	if (!MakeLeft(pParant, pLeft))
 		printf("MakeLeft Fail!!");
@@ -73,4 +134,9 @@ void main()
 	pSeed = pParant;
 
 	Print(pSeed);
+}
+
+void main()
+{
+	SetTestMain();
 }
--- ProgramingBasic/STL/STL/STL/STL.cpp
+++ ProgramingBasic/STL/STL/STL/STL.cpp
@@ -125,7 +125,7 @@ void ListMain()
 	for (it = container.begin(); it != container.end(); it++)
 		cout << *it << ",";
 	cout << endl;
-	
+	it = container.begin();
 	cout << "begin:" << *it << endl;
 	it++; it++;
 	cout << "begin+2:" << *it << endl;
@@ -153,8 +153,123 @@ void ListMain()
 	container.clear(); //모두삭제
 }
 
+void QueueMain()
+{
+	queue<int> container;
+
+	container.push(10);
+	container.push(50);
+	container.push(20);
+	container.push(30);
+	container.push(60);
+
+	while (container.empty() == false)
+	{
+		cout << container.front() << endl;
+		container.pop();
+	}
+}
+
+void StatckMain()
+{
+	stack<int> container;
+
+	container.push(10);
+	container.push(50);
+	container.push(20);
+	container.push(30);
+	container.push(60);
+
+	while (container.empty() == false)
+	{
+		cout << container.top() << endl;
+		container.pop();
+	}
+}
+
+void PriorityQuequeMain()
+{
+	priority_queue<int> container;
+
+	container.push(10);
+	container.push(50);
+	container.push(20);
+	container.push(30);
+	container.push(60);
+
+	while (container.empty() == false)
+	{
+		cout << container.top() << endl;
+		container.pop();
+	}
+}
+
+void SetMain()
+{
+	set<int> container;
+
+	for (int i = 0; i < 7; i++)
+	{
+		container.insert((i+1)*10);
+	}
+
+	set<int>::iterator it = container.begin();
+	for (; it != container.end(); it++)
+		cout << *it << ",";
+	cout << endl;
+}
+
+void MapMain()
+{
+	map<int, string> container;
+
+	container[0] = "Zero";
+	container[1] = "One";
+	container[2] = "Two";
+	container[3] = "Three";
+	container[4] = "Four";
+	container[5] = "Five";
+	container[6] = "Six";
+
+	for (int i = 0; i < container.size(); i++)
+	{
+		cout << i << ":" << container[i] << ",";
+	}
+	cout << endl;
+
+	map<int, string>::iterator it = container.begin();
+	for (; it != container.end(); it++)
+		cout << it->first << ":"<< it->second << ",";
+	cout << endl;
+}
+
+void HashMapMain()
+{
+	unordered_map<int, string> container;
+
+	container[0] = "Zero";
+	container[1] = "One";
+	container[2] = "Two";
+	container[3] = "Three";
+	container[4] = "Four";
+	container[5] = "Five";
+	container[6] = "Six";
+
+	for (int i = 0; i < container.size(); i++)
+	{
+		cout << i << ":" << container[i] << ",";
+	}
+	cout << endl;
+}
+
 void main()
 {
 	//VectorMain();
-	DequeMain();
+	//DequeMain();
+	//ListMain();
+	//QueueMain();
+	//StatckMain();
+	//PriorityQuequeMain();
+	//SetMain();
+	MapMain();
 }
