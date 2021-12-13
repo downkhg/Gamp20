8975eb56c826a53edfec8eb70797495a494627c9
downkhg <downkhg@gmail.com>
2021-12-13 17:51:53 +0900

    GUI장면전환하기

--- CompterGraphics/ComputerGraphics/Assets/Scripts/GameManager.cs
+++ CompterGraphics/ComputerGraphics/Assets/Scripts/GameManager.cs
@@ -26,6 +26,61 @@ public class GameManager : MonoBehaviour
         return m_cInstance;
     }
 
+    public enum E_GUI_STATE { NONE = -1, TITLE, GAMEOVER, THEEND, PLAY }
+
+    [SerializeField]
+    List<GameObject> m_listGUIScenes;
+    [SerializeField]
+    E_GUI_STATE m_eGurGUIState;
+
+    void ShowGUIScenes(int idx)
+    {
+        for(int i = 0;  i < m_listGUIScenes.Count; i++)
+        {
+            if(i == idx)
+                m_listGUIScenes[i].SetActive(true);
+            else
+                m_listGUIScenes[i].SetActive(false);
+        }
+    }
+
+    void SetState(E_GUI_STATE state)
+    {
+        switch(state)
+        {
+            case E_GUI_STATE.TITLE:
+                break;
+            case E_GUI_STATE.GAMEOVER:
+                break;
+            case E_GUI_STATE.THEEND:
+                break;
+            case E_GUI_STATE.PLAY:
+                break;
+        }
+        ShowGUIScenes((int)state);
+        m_eGurGUIState = state;
+    }
+
+    void UpdateState()
+    {
+        switch (m_eGurGUIState)
+        {
+            case E_GUI_STATE.TITLE:
+                break;
+            case E_GUI_STATE.GAMEOVER:
+                break;
+            case E_GUI_STATE.THEEND:
+                break;
+            case E_GUI_STATE.PLAY:
+                break;
+        }
+    }
+
+    public void EventGUIState(int idx)
+    {
+        SetState((E_GUI_STATE)idx);
+    }
+
     private void Awake()
     {
         m_cInstance = this;
@@ -43,9 +98,8 @@ public class GameManager : MonoBehaviour
         Debug.Log("GameManager::Start() 1");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateState();
     }
 }
